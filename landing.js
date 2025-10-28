e04e3c980001ee6817cd624a5f8d48568741b5a0
flexx254 <67399047+flexx254@users.noreply.github.com>
2025-10-28 15:02:52 +0300

    Implement toggle functionality for menu

--- landing.js
+++ landing.js
@@ -1,1 +1,8 @@
+// Toggle menu open/close
+const menuIcon = document.getElementById("menuIcon");
+const navLinks = document.getElementById("navLinks");
 
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
